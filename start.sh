d5acc3e16ac947768086765f40673a883c63d0c2
Joan Marc Carbo Arnau <joanmarc@gmail.com>
2017-12-18 12:38:13 +0100

    restore cups backup

--- start.sh
+++ start.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+if [ -f "/data/cups.tgz" ]
+then
+  echo "Updating cups config."
+  tar zxvpf /data/cups.tgz
+fi
 echo "Starting CUPS in background..."
 /etc/init.d/dbus start
 #/etc/init.d/cups start
